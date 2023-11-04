329f1900993fa1653359e15930b230a238042b99
Zee Vasilyev <zeejfps@gmail.com>
2023-11-03 22:03:00 -0500

    Made new tetromino spawn when the last one hits the ground;

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,9 +1,9 @@
-using System;
 using System.Collections;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField] private Tetromino m_TetrominoPrefab;
     [SerializeField] private Tetromino m_Tetromino;
 
     private void Start()
@@ -19,6 +19,8 @@ public class GameController : MonoBehaviour
             m_Tetromino.TryMoveRight();
         else if (Input.GetKeyDown(KeyCode.R))
             m_Tetromino.TryRotate();
+        else if (Input.GetKeyDown(KeyCode.S))
+            m_Tetromino.TryMoveDown();
     }
     
     private IEnumerator MoveDownRoutine()
@@ -26,7 +28,10 @@ public class GameController : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(0.5f);
-            m_Tetromino.TryMoveDown();
+            if (!m_Tetromino.TryMoveDown())
+            {
+                m_Tetromino = Instantiate(m_TetrominoPrefab);
+            }
         }
     }
 }
