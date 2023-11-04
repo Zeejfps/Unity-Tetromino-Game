3086c2db1ebc12d589c8298b0b70b98e3bd88a2d
Zee Vasilyev <zeejfps@gmail.com>
2023-11-04 16:07:22 -0500

    Added some basic text animation

--- Assets/_Tetris/Scripts/Hud.cs
+++ Assets/_Tetris/Scripts/Hud.cs
@@ -1,14 +1,16 @@
-using System;
+using EnvDev;
 using TMPro;
 using UnityEngine;
 
-public sealed class Hud : MonoBehaviour
+public sealed class Hud : MonoBehaviour, IAnimate
 {
     [SerializeField] private GameScoreProvider m_GameScoreProvider;
     [SerializeField] private TMP_Text m_ScoreText;
 
     private IGameScore m_GameScore;
 
+    private int m_PrevPointCount;
+    
     private void Awake()
     {
         m_GameScore = m_GameScoreProvider.Get();
@@ -17,6 +19,8 @@ public sealed class Hud : MonoBehaviour
     private void Start()
     {
         m_GameScore.PointsChanged += GameScore_OnPointsChanged;
+        m_PrevPointCount = m_GameScore.TotalPoints;
+        m_ScoreText.text = m_PrevPointCount.ToString();
     }
 
     private void OnDestroy()
@@ -26,6 +30,15 @@ public sealed class Hud : MonoBehaviour
 
     private void GameScore_OnPointsChanged(int pointsScored)
     {
-        m_ScoreText.text = m_GameScore.TotalPoints.ToString();
+        var targetPointCount = m_GameScore.TotalPoints;
+        this.Animate(0.5f, EaseFunctions.SineInOut, t =>
+        {
+            m_ScoreText.text = Mathf.LerpUnclamped(m_PrevPointCount, targetPointCount, t).ToString("0");
+        }, () =>
+        {
+            m_PrevPointCount = m_GameScore.TotalPoints;
+        });
     }
+
+    public Coroutine AnimationRoutine { get; set; }
 }
