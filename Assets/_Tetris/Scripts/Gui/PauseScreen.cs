b574efb23115c5c6811782e77bb8e9216828bd5c
Zee Vasilyev <zeejfps@gmail.com>
2023-11-05 09:28:04 -0600

    Added a Restart Game Button

--- Assets/_Tetris/Scripts/Gui/PauseScreen.cs
+++ Assets/_Tetris/Scripts/Gui/PauseScreen.cs
@@ -5,6 +5,7 @@ public sealed class PauseScreen : MonoBehaviour
 {
     [SerializeField] private GameStateMachineProvider m_GameStateMachineProvider;
     [SerializeField] private Button m_ResumeButton;
+    [SerializeField] private Button m_RestartButton;
 
     private IGameStateMachine m_GameStateMachine;
 
@@ -16,6 +17,7 @@ public sealed class PauseScreen : MonoBehaviour
     private void Start()
     {
         m_ResumeButton.onClick.AddListener(ResumeButton_OnClicked);
+        m_RestartButton.onClick.AddListener(RestartButton_OnClicked);
         m_GameStateMachine.StateChanged += GameStateMachine_OnStateChanged;
         gameObject.SetActive(m_GameStateMachine.State == GameState.Paused);
     }
@@ -23,7 +25,14 @@ public sealed class PauseScreen : MonoBehaviour
     private void OnDestroy()
     {
         m_ResumeButton.onClick.RemoveListener(ResumeButton_OnClicked);
+        m_RestartButton.onClick.RemoveListener(RestartButton_OnClicked);
         m_GameStateMachine.StateChanged -= GameStateMachine_OnStateChanged;
+    }
+
+    private void RestartButton_OnClicked()
+    {
+        m_GameStateMachine.TransitionTo(GameState.GameOver);
+        m_GameStateMachine.TransitionTo(GameState.Playing);
     }
 
     private void ResumeButton_OnClicked()
